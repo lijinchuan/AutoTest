2a44c373fb8284634328dd717ab868f12a7c1fdb
Tom <c2010@topuc.com>
2021-12-01 18:36:27 +0800

    添加测试用例窗口

---
+++ AutoTest.UI/SubForm/UpsertTestCaseDlg.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace AutoTest.UI.SubForm
+{
+    partial class UpsertTestCaseDlg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myTabControl1 = new AutoTest.UI.UC.MyTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBUrl = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.myTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myTabControl1
+            // 
+            this.myTabControl1.Controls.Add(this.tabPage1);
+            this.myTabControl1.Controls.Add(this.tabPage2);
+            this.myTabControl1.Controls.Add(this.tabPage3);
+            this.myTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myTabControl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.myTabControl1.ItemSize = new System.Drawing.Size(0, 18);
+            this.myTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.myTabControl1.Name = "myTabControl1";
+            this.myTabControl1.SelectedIndex = 0;
+            this.myTabControl1.Size = new System.Drawing.Size(1140, 740);
+            this.myTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.myTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.TBUrl);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1132, 714);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "基本配置";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1132, 714);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "测试脚本";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(18, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地址：";
+            // 
+            // TBUrl
+            // 
+            this.TBUrl.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.TBUrl.Location = new System.Drawing.Point(93, 32);
+            this.TBUrl.Name = "TBUrl";
+            this.TBUrl.Size = new System.Drawing.Size(1012, 30);
+            this.TBUrl.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1132, 714);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "验证脚本";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // UpsertTestCaseDlg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1140, 740);
+            this.Controls.Add(this.myTabControl1);
+            this.Name = "UpsertTestCaseDlg";
+            this.Text = "UpsertTestCaseDlg";
+            this.myTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UC.MyTabControl myTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox TBUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
