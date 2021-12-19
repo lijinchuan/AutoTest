58a6711aeb90de57f135b08c7922f1f3693f5aaf
ljc <403479851@qq.com>
2021-12-19 15:00:02 +0800

    测试报告

---
+++ AutoTest.Domain/Entity/TestReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest.Domain.Entity
+{
+    public class TestReport
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public DateTime TestTime
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ AutoTest.Domain/Entity/TestReportDetail.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest.Domain.Entity
+{
+    public class TestReportDetail
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public int SorceId
+        {
+            get;
+            set;
+        }
+
+        public int SiteId
+        {
+            get;
+            set;
+        }
+
+        public int PageId
+        {
+            get;
+            set;
+        }
+
+        public int ReportId
+        {
+            get;
+            set;
+        }
+
+        public int TestCaseId
+        {
+            get;
+            set;
+        }
+
+        public TestEnv TestEnv
+        {
+            get;
+            set;
+        }
+
+        public TestEnvParam TestEnvParam
+        {
+            get;
+            set;
+        }
+
+        public TestCase TestCase
+        {
+            get;
+            set;
+        }
+
+        public TestResult TestResult
+        {
+            get;
+            set;
+        }
+    }
+}
