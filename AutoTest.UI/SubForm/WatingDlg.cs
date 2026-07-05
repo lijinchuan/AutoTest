d6e7b8fd083820c7e01bb77f6ebe0169864f2f38
Administrator <Administrator@MS-XJSOQJENFZUC>
2026-07-05 14:47:33 +0800

    fix: 优化启动崩溃问题

--- AutoTest.UI/SubForm/WatingDlg.cs
+++ AutoTest.UI/SubForm/WatingDlg.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AutoTest.UI.SubForm
@@ -16,42 +10,65 @@ namespace AutoTest.UI.SubForm
         public WatingDlg()
         {
             InitializeComponent();
-            WatingDlg.CheckForIllegalCrossThreadCalls = false;
+
+            // 允许跨线程更新 UI：
+            // 对话框由后台线程的消息泵驱动，主线程在工作期间需要
+            // 通过 Msg 属性直接更新界面文字（主线程可能处于阻塞状态）
+            CheckForIllegalCrossThreadCalls = false;
 
             this.loadingBox1.Location = new Point(0, 0);
             this.Width = this.loadingBox1.Width;
             this.Height = this.loadingBox1.Height;
         }
 
+        /// <summary>
+        /// 显示等待对话框。
+        /// 主线程先打开对话框确保可靠显示，随后后台线程接管消息泵，
+        /// 使主线程可继续执行而对话框保持响应（GIF 动画、文字更新）。
+        /// </summary>
         public void Show(string msg)
         {
             this.Msg = msg;
-            new Thread(new ThreadStart(() =>
+
+            // 后台线程：短暂延迟后接管对话框的消息泵
+            var bg = new Thread(() =>
             {
-                Thread.Sleep(200);
-                this.DialogResult = DialogResult.Cancel;
-                this.Visible = false;
-                Thread.Sleep(200);
-                this.ShowDialog();
+                for (var i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        Thread.Sleep(100);
+                        this.DialogResult = DialogResult.Cancel;  // 释放主线程的 ShowDialog
+                        Thread.Sleep(100);
+                        this.ShowDialog();                        // 后台线程接管
+                        break;
 
-            })).Start();
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            });
+            bg.IsBackground = true;
+            bg.SetApartmentState(ApartmentState.STA);
+            bg.Start();
+
+            // 主线程阻塞在这里，确保对话框可靠显示
             this.ShowDialog();
-
         }
 
+        /// <summary>
+        /// 关闭对话框。
+        /// </summary>
         public new void Hide()
         {
-            Thread.Sleep(500);
-            this.DialogResult = DialogResult.None;
-            this.Visible = false;
+            this.DialogResult = DialogResult.Cancel;
         }
 
         public string Msg
         {
-            set
-            {
-                this.loadingBox1.Msg = value;
-            }
+            set => this.loadingBox1.Msg = value;
         }
     }
 }
